7b2cab9ab678e016b48cb3132c9cdc5a96843a98
Abdul <nav20202@gmail.com>
2018-03-27 11:40:36 +0530

    Made all scripts and image resources local and arranged files into folders

--- script/bot.js
+++ script/bot.js
@@ -15,10 +15,10 @@ $(document).on('click', '.panel-heading', function (e) {
 
 //CHATBOT JS
 var me = {};
-me.avatar = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
+me.avatar = ""; //USER AVTAR HERE
 
 var you = {};
-you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
+you.avatar = "Avtar_48x48.jpg";
 
 function formatAMPM(date) {
     var hours = date.getHours();
